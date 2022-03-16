473bcab0186e3f0734dd3943c6c56a6d50a586fa
Naveen Natarajan <naveen.natarajan@cesltd.com>
2022-03-16 12:33:24 +0530

    Ignored Files

---
+++ XamarinApp/XamarinApp/Services/DBConnection.cs
@@ -0,0 +1,65 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using XamarinApp.Models;
+using XamarinApp.Services.Interfaces;
+
+namespace XamarinApp.Services
+{
+    public class DBConnection
+    {
+        private readonly SQLiteAsyncConnection _db;
+
+        //public static readonly AsyncLazy<DBConnection> Instance = new AsyncLazy<DBConnection>(async () =>
+        //{
+        //    var instance = new DBConnection();
+        //    CreateTableResult result = await Database.CreateTableAsync<TodoItem>();
+        //    return instance;
+        //});
+
+        //public DBConnection()
+        //{
+        //    _db = new SQLiteAsyncConnection(App.DatabasePath, App.Flags);
+        //}
+    
+        public DBConnection(string dbPath)
+        {
+            _db = new SQLiteAsyncConnection(dbPath);
+            _db.CreateTableAsync<UserModel>().Wait();
+        }
+
+        public Task<List<UserModel>> GetUsers()
+        {
+            return _db.Table<UserModel>().ToListAsync();
+        }
+
+        public Task<UserModel> FindUser(UserModel user)
+        {
+            return _db.Table<UserModel>().Where(x => x.UserName == user.UserName && x.Password == user.Password).FirstOrDefaultAsync();
+            //var result = _db.Table<UserModel>().Where(x => x.UserName == user.UserName && x.Password == user.Password).ToListAsync();
+            //var alldata = _db.Table<UserModel>().ToListAsync();
+            //if (result != null)
+            //{
+            //    return true;
+            //}
+
+            //return false;
+        }
+
+        public Task<int> SaveUser(UserModel user)
+        {
+            if(user.ID != 0)
+            {
+                return _db.UpdateAsync(user);
+            }
+            else
+            {
+                return _db.InsertAsync(user);
+            }            
+        }
+    }
+}
---
+++ XamarinApp/XamarinApp/Services/Interfaces/IUserLoginService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using XamarinApp.Models;
+
+namespace XamarinApp.Services.Interfaces
+{
+    public interface IUserLoginService
+    {
+        Task<bool> LoginUser(UserModel user);
+    }
+}
